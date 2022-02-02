02d575184a442a2db9499cd20ae1684a1f04ce0a
Ryan Ashby <rjapsu160@gmail.com>
2022-02-02 09:35:10 -0800

    first test pass, creating haiku object
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>

--- __tests__/haiuk.test.js
+++ __tests__/haiuk.test.js
@@ -4,7 +4,9 @@ import Haiku from "../src/haiku";
 describe("Haiku", () => {
 
   test("should create a haiku object with lines 1, 2, and 3", () => {
-    const myHaiku = new Haiku(line1, line2, line3); 
-
-  })
-})
+    const myHaiku = new Haiku("Hello There", "How are you", "I'm fine"); 
+    expect(myHaiku.line1).toEqual("Hello There");
+    expect(myHaiku.line2).toEqual("How are you");
+    expect(myHaiku.line3).toEqual("I'm fine");
+  });
+});
