1c9f97a8afffbb1102a1b7b4caf6e45aa65ee2ea
Ryan Ashby <rjapsu160@gmail.com>
2022-02-02 10:31:51 -0800

    pass test removing punct and creating array of words from lines
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -4,17 +4,17 @@ import Haiku from "../src/haiku";
 describe("Haiku", () => {
 
   test("should create a haiku object with lines 1, 2, and 3", () => {
-    const myHaiku = new Haiku("Hello There", "How are you", "I'm fine"); 
-    expect(myHaiku.line1).toEqual("Hello There");
-    expect(myHaiku.line2).toEqual("How are you");
+    const myHaiku = new Haiku("Hello There!", "How are you?", "I'm fine"); 
+    expect(myHaiku.line1).toEqual("Hello There!");
+    expect(myHaiku.line2).toEqual("How are you?");
     expect(myHaiku.line3).toEqual("I'm fine");
   });
 
   test("should create three arrays of words for each line", () => {
-    const myHaiku = new Haiku("Hello There", "How are you", "I'm fine"); 
+    const myHaiku = new Haiku("Hello There!", "How are you?", "I'm fine"); 
     myHaiku.splitLine();
     expect(myHaiku.line1).toEqual(["hello","there"]);
     expect(myHaiku.line2).toEqual(["how","are","you"]);
-    expect(myHaiku.line3).toEqual(["i'm","fine"]);
+    expect(myHaiku.line3).toEqual(["im","fine"]);
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,16 +1,24 @@
 export default class Haiku {
-  constructor (line1, line2, line3) {
+
+  constructor(line1, line2, line3) {
     this.line1 = line1;
     this.line2 = line2;
     this.line3 = line3;
+    console.log(this);
   }
 
   splitLine() {
-
+    let array1 = this.line1.toLowerCase().replace(/[.,\/#?!$%\^&\*;:{}=\-_'`~()]/g,"").replace(/\s{2,}/g," ").split(" ");
+    this.line1 = array1;
+    let array2 = this.line2.toLowerCase().replace(/[.,\/#?!$%\^&\*;:{}=\-_'`~()]/g,"").replace(/\s{2,}/g," ").split(" ");
+    this.line2 = array2;
+    let array3 = this.line3.toLowerCase().replace(/[.,\/#?!$%\^&\*;:{}=\-_'`~()]/g,"").replace(/\s{2,}/g," ").split(" ");
+    this.line3 = array3;
+    console.log(this.line2);
   }
 }
-
-
+const myHaiku = new Haiku("Hello There!", "How are you?", "I'm fine");
+myHaiku.splitLine();
 // let string = "This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation";
 // let punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 // let finalString = punctuationless.replace(/\s{2,}/g," ");
