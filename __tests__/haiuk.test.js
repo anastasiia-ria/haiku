8d047915dd8e0f62c2b5c9e0c2206c3d0ba89496
Ryan Ashby <rjapsu160@gmail.com>
2022-02-02 09:24:55 -0800

    write test for haiku constructor/good fail
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- __tests__/haiuk.test.js
+++ __tests__/haiuk.test.js
@@ -4,7 +4,7 @@ import Haiku from "../src/haiku";
 describe("Haiku", () => {
 
   test("should create a haiku object with lines 1, 2, and 3", () => {
-    const myHaiku = new Haiku(line1, line2, line3);
+    const myHaiku = new Haiku(line1, line2, line3); 
 
   })
 })
