6278dd2e2fcd0718c296ff2ea6418c52443c9f0b
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-03 21:42:14 -0800

    update tests for the Line()
    Co-authored-by: Ryan Ashby <rjapsu160@gmail.com>

--- __tests__/line.test.js
+++ __tests__/line.test.js
@@ -7,24 +7,24 @@ describe("Line", () => {
   let line3;
 
   beforeEach(() => {
-    line1 = new Line("Happy there Francis!"); 
+    line1 = new Line("Happy goes Francis!");
     line2 = new Line("How are you doing today?");
     line3 = new Line("I am fine thank you");
   });
 
   test("should create three lines objects ", () => {
-    expect(line1.line).toEqual("Happy there Francis!");
+    expect(line1.line).toEqual("Happy goes Francis!");
     expect(line2.line).toEqual("How are you doing today?");
     expect(line3.line).toEqual("I am fine thank you");
   });
 
-  test("should create three arrays of words for each line", () => { 
+  test("should create three arrays of words for each line", () => {
     line1.splitLine();
     line2.splitLine();
     line3.splitLine();
-    expect(line1.lineArray).toEqual(["happy","there","francis"]);
-    expect(line2.lineArray).toEqual(["how","are","you","doing","today"]);
-    expect(line3.lineArray).toEqual(["i","am","fine","thank","you"]);
+    expect(line1.lineArray).toEqual(["happy", "goes", "francis"]);
+    expect(line2.lineArray).toEqual(["how", "are", "you", "doing", "today"]);
+    expect(line3.lineArray).toEqual(["i", "am", "fine", "thank", "you"]);
   });
 
   test("should remove silent vowels", () => {
@@ -34,9 +34,9 @@ describe("Line", () => {
     line1.subtractVowels();
     line2.subtractVowels();
     line3.subtractVowels();
-    expect(line1.lineArray).toEqual(["happy","ther","francis"]);
-    expect(line2.lineArray).toEqual(["how","ar","yo","doing","today"]);
-    expect(line3.lineArray).toEqual(["i","am","fin","thank","yo"]);
+    expect(line1.lineArray).toEqual(["happy", "go", "francis"]);
+    expect(line2.lineArray).toEqual(["how", "ar", "u", "doing", "tody"]);
+    expect(line3.lineArray).toEqual(["i", "am", "fin", "thank", "u"]);
   });
 
   test("should return the count of vowels per each word for each line", () => {
@@ -56,12 +56,11 @@ describe("Line", () => {
 
   test("should continue to push new random words into line1 array, until 5 syllables are reached. if over 5 syllables, pop off word and try again until 5 syllables are reached", () => {
     let randomLine1 = new Line("");
-    expect(randomLine1.randomFive()).toEqual(true);
-  })
+    expect(randomLine1.randomHaiku(5)).toEqual(true);
+  });
 
   test("should continue to push new random words into line2 array, until 7 syllables are reached. if over 7 syllables, pop off word and try again until 7 syllables are reached", () => {
     let randomLine2 = new Line("");
-    expect(randomLine2.randomSeven()).toEqual(true);
-  })
+    expect(randomLine2.randomHaiku(7)).toEqual(true);
+  });
 });
-
--- src/line.js
+++ src/line.js
@@ -1,5 +1,4 @@
 export default class Line {
-
   constructor(line) {
     this.line = line;
     this.lineArray = [];
@@ -8,58 +7,66 @@ export default class Line {
   }
 
   splitLine() {
-    this.lineArray = this.line.toLowerCase().replace(/[.,/#?!$%^&*;:{}=\-_'`~()]/g,"").replace(/\s{2,}/g," ").split(" ");
+    this.lineArray = this.line
+      .toLowerCase()
+      .replace(/[.,/#?!$%^&*;:{}=\-_'`~()]/g, "")
+      .replace(/\s{2,}/g, " ")
+      .split(" ");
   }
 
   subtractVowels() {
-    const vowelArray = ["a", "e", "i", "o", "u"];
+    const vowelArray = ["a", "e", "i", "o", "u", "y"];
     for (let i = 0; i < this.lineArray.length; i++) {
       let vowelCheck = 0;
       let wordArray = this.lineArray[i].split("");
-      for (let j = 0; j < wordArray.length; j ++) {
+
+      for (let j = 0; j < wordArray.length; j++) {
         if (vowelArray.includes(wordArray[j])) {
-          vowelCheck ++;
+          vowelCheck++;
         }
-        if (vowelArray.includes(wordArray[j]) && vowelArray.includes(wordArray[j+1])) {
-          if ((wordArray[j+3] === "g") && wordArray[j+2] === "n" && wordArray[j+1] === "i") {
-            return 0;
-          } else {
-            wordArray.splice((j+1), 1);
+      }
+
+      let eEnding = wordArray[wordArray.length - 1] === "e" && vowelCheck > 1;
+      let esEdEnding = (wordArray[wordArray.length - 1] === "s" || wordArray[wordArray.length - 1] === "d") && wordArray[wordArray.length - 2] === "e" && vowelCheck > 1;
+
+      if (eEnding) {
+        wordArray.pop();
+      }
+      if (esEdEnding) {
+        wordArray.pop();
+        wordArray.pop();
+      }
+
+      for (let j = 0; j < wordArray.length; j++) {
+        let doubleVowel = vowelArray.includes(wordArray[j]) && vowelArray.includes(wordArray[j + 1]);
+        let ingEnding = wordArray[j + 1] === "i" && wordArray[j + 2] === "n" && wordArray[j + 3] === "g";
+
+        if (vowelArray.includes(wordArray[j])) {
+          vowelCheck++;
+        }
+
+        if (doubleVowel) {
+          if (!ingEnding) {
+            wordArray.splice(j, 1);
+            j--;
           }
-        } 
-        if (wordArray[j] === "y" && ((!vowelArray.includes(wordArray[j-1])) || wordArray[j-1] === undefined ) && ((!vowelArray.includes(wordArray[j+1])) || wordArray[j-1] === undefined )) {
-          this.sylCount ++;
-          console.log(wordArray[j-1]);
-          console.log(wordArray[j+1]);
-          console.log(vowelArray.includes(wordArray[j-1]));
-          console.log(vowelArray.includes(wordArray[j+1]));
-          console.log("yCount");
         }
-      }
-      if ("e" === wordArray[wordArray.length-1] && (vowelCheck > 1 || this.count === 1)) {
-        wordArray.pop();
-      }
-      if (("s" === wordArray[wordArray.length-1] || "d" === wordArray[wordArray.length-1] )&& "e" === wordArray[wordArray.length-2] && vowelCheck > 1) {
-        wordArray.pop();
-        wordArray.pop();
       }
       this.lineArray[i] = wordArray.join("");
     }
   }
 
   countVowels() {
-    let vowels = /[aeiou]/gi;
+    let vowels = /[aeiouy]/gi;
     let that = this;
-    this.sylCount = 0;
+    that.sylCount = 0;
 
-    this.lineArray.forEach(function(word) {
+    this.lineArray.forEach(function (word) {
       let result = word.match(vowels);
       if (result !== null) {
         that.sylCount += result.length;
       }
-      console.log(word);
     });
-    console.log(that.sylCount);
   }
 
   countSyllables() {
@@ -67,38 +74,20 @@ export default class Line {
     this.countVowels();
   }
 
-  randomFive() {
-    let randomWords = require('random-words');
-    for ( let i = 0; i < 5; i++) {
-      let randomWord = randomWords({exactly: 1}).join("");
+  randomHaiku(syllables) {
+    let randomWords = require("random-words");
+    for (let i = 0; i < syllables; i++) {
+      let randomWord = randomWords({ exactly: 1 }).join("");
       this.lineArray.push(randomWord);
       this.lineArrayTest.push(randomWord);
       this.countSyllables();
-      if (this.sylCount > 5) {
+      console.log(this.sylCount);
+      if (this.sylCount > syllables) {
         this.lineArray.pop();
         this.lineArrayTest.pop();
         this.countSyllables();
         i--;
-      } else if (this.sylCount === 5) {
-        this.line = this.lineArrayTest.join(" ");
-        return true;
-      }
-    }
-  }
-
-  randomSeven() {
-    let randomWords = require('random-words');
-    for ( let i = 0; i < 7; i++) {
-      let randomWord = randomWords({exactly: 1}).join("");
-      this.lineArray.push(randomWord);
-      this.lineArrayTest.push(randomWord);
-      this.countSyllables();
-      if (this.sylCount > 7) {
-        this.lineArray.pop();
-        this.lineArrayTest.pop();
-        this.countSyllables();
-        i--;
-      } else if (this.sylCount === 7) {
+      } else if (this.sylCount === syllables) {
         this.line = this.lineArrayTest.join(" ");
         return true;
       }
