f732a607901ee689cac46007292317ae59648bbf
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-03 22:28:21 -0800

    update ui logic
    Co-authored-by: Ryan Ashby <rjapsu160@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-//import Haiku from './haiku';
-import Line from './line';
-import HaikuCreator from './haikuCreator';
+import $ from "jquery";
+import "./css/styles.css";
+import Line from "./js/line";
+import Haiku from "./js/haiku";
 
-let checkHaiku = new HaikuCreator();
-let randomHaiku = new HaikuCreator();
+let checkHaiku = new Haiku();
+
+function showHaiku(haikuToDisplay) {
+  $("ul#display-haiku").empty();
+  for (let i = 0; i < 3; i++) {
+    $("ul#display-haiku").append("<li>" + haikuToDisplay.haiku[i].line.toLowerCase().replace(/[.,/#?!$%^&*;:{}=\-_'`~()]/g, "") + "</li>");
+  }
+}
+
+function randomFunction() {
+  let randomOne = new Line("");
+  let randomTwo = new Line("");
+  let randomThree = new Line("");
+  let randomHaiku = new Haiku();
+  randomOne.randomHaiku(5);
+  randomTwo.randomHaiku(7);
+  randomThree.randomHaiku(5);
+  randomHaiku.addLine(randomOne);
+  randomHaiku.addLine(randomTwo);
+  randomHaiku.addLine(randomThree);
+
+  showHaiku(randomHaiku);
+}
+
+randomFunction();
 
 $(document).ready(function () {
-  $("form#haiku-checker").submit(function(event) {
+  $("form#haiku-checker").submit(function (event) {
     event.preventDefault();
-
     const inputtedLineOne = $("input#line-one").val();
     const inputtedLineTwo = $("input#line-two").val();
     const inputtedLineThree = $("input#line-three").val();
-
     $("input#line-one").val("");
     $("input#line-two").val("");
     $("input#line-three").val("");
-
     checkHaiku.haiku = [];
-    let lineOne = new Line (inputtedLineOne);
-    let lineTwo = new Line (inputtedLineTwo);
-    let lineThree = new Line (inputtedLineThree);
+    let lineOne = new Line(inputtedLineOne);
+    let lineTwo = new Line(inputtedLineTwo);
+    let lineThree = new Line(inputtedLineThree);
     checkHaiku.addLine(lineOne);
     checkHaiku.addLine(lineTwo);
     checkHaiku.addLine(lineThree);
@@ -34,32 +51,19 @@ $(document).ready(function () {
     lineOne.countSyllables();
     lineTwo.countSyllables();
     lineThree.countSyllables();
-
-    $('#check-result').empty();
+    $("#check-result").empty();
     if (checkHaiku.isAHaiku()) {
-      $('#check-result').html("It is a haiku");
+      showHaiku(checkHaiku);
+      $("#check-result").html("It is a haiku");
     } else {
-      $('#check-result').html("It is not a haiku");
+      showHaiku(checkHaiku);
+      $("#check-result").html("It is not a haiku");
     }
+    setTimeout(function () {
+      $("#check-result").empty();
+    }, 1500);
   });
-
-  $("#random").click(function() {
-    let randomOne = new Line ("");
-    let randomTwo = new Line ("");
-    let randomThree = new Line ("");
-    randomOne.randomFive();
-    randomTwo.randomSeven();
-    randomThree.randomFive();
-    randomHaiku.addLine(randomOne);
-    randomHaiku.addLine(randomTwo);
-    randomHaiku.addLine(randomThree);
-
-    $('ul#random-haiku').empty();
-    $('ul#random-haiku').append("<li>" + randomOne.line + "</li>");
-    $('ul#random-haiku').append("<li>" + randomTwo.line + "</li>");
-    $('ul#random-haiku').append("<li>" + randomThree.line + "</li>");
-    $(".output").fadeIn();
-    window.scrollTo(0,document.body.scrollHeight);
+  $("#random").click(function () {
+    randomFunction();
   });
-
 });
